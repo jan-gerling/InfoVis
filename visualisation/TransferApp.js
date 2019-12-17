09d5e67e1faeee08e6eea3f5c3546f201989304d
Jelle Eysbach <J.T.Eysbach@student.tudelft.nl>
2019-12-17 14:24:16 +0100

    Added seperator lines between players

--- visualisation/TransferApp.js
+++ visualisation/TransferApp.js
@@ -17,10 +17,12 @@ function handleMouseEnter(transfer) {
   var tiny = document.getElementById("tinyWindow");
   var tinyName = document.getElementById("tinyWindow_name");
   var tinyAge = document.getElementById("tinyWindow_age");
+  var tinyAmount = document.getElementById("tinyWindow_value");
   var tinyImage = document.getElementById("tinyImage");
   tinyImage.src = transfer.player.image;
   tinyName.innerHTML = transfer.player.name;
   tinyAge.innerHTML = transfer.player.age;
+  tinyAmount.innerHTML = transfer.transfer.value;
   tiny.style.display = "block";
 }
 
@@ -53,63 +55,6 @@ function getPlayerValue(val) {
     }
   }
   return res;
-}
-
-function drawClubBarchartLeagues(svg, width, height) {
-  var data = transfer_data["2004-2005"].concat(transfer_data["2003-2004"]).concat(transfer_data["2005-2006"]);
-  function checkClub(x) {
-    return x.from.name === "Man Utd";
-  }
-  data = data.filter(checkClub);
-  var seenTransfers = {};
-
-  data = data.filter(function(currentObject) {
-    if (currentObject.player.name in seenTransfers) {
-        return false;
-    } else {
-        seenTransfers[currentObject.player.name] = true;
-        return true;
-    }
-  });
-
-  var leagueData = {};
-  data.forEach(function (val) {
-    if (val.to.league in leagueData) {
-      leagueData[val.to.league].amount += 1;
-      leagueData[val.to.league].value += getPlayerValue(val.transfer.value);
-      leagueData[val.to.league].transfers.push(val);
-    } else {
-      leagueData[val.to.league] = {name: val.to.league, amount: 1, value: getPlayerValue(val.transfer.value), transfers: [val]};
-    }
-  });
-  var data = Object.values(leagueData);
-
-  var x = d3.scaleBand().range([0, width]);
-  var y = d3.scaleLinear().range([height, 0]);
-
-  x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  var xAxis = d3.axisBottom()
-      .scale(x);
-
-  var yAxis = d3.axisLeft()
-      .scale(y);
-
-  svg.append("g")
-    .call(yAxis);
-  svg.append("g")
-    .call(xAxis)
-    .attr("transform", "translate(0, " + height + ")");
-
-  var bars = svg.selectAll("bar")
-    .data(data);
-  bars.enter().append("rect")
-    .attr("fill", "steelblue")
-    .attr("x", function(d) { return x(d.name); })
-    .attr("width", x.bandwidth())
-    .attr("y", function(d) { return y(d.value); })
-    .attr("height", function(d) { return height - y(d.value); });
 }
 
 function drawClubBarchartClub(svg, width, height, options) {
@@ -183,6 +128,7 @@ function drawClubBarchartClub(svg, width, height, options) {
     .call(xAxis)
     .attr("transform", "translate(0, " + height + ")");
 
+  var seperatorHeights = [];
   var heightCounter = {};
   for (var i = 0; i < ogdata.length; i++) {
     var f = ogdata[i].to.name === options.club;
@@ -194,10 +140,12 @@ function drawClubBarchartClub(svg, width, height, options) {
   var bars = svg.selectAll("bar")
     .data(sortedData).enter();
   bars.append("rect")
-    .attr("fill", function(d) { return d.to.name === options.club ? "steelblue" : "red"; })
+    // Categorical colors found on https://vega.github.io/vega/docs/schemes/#categorical
+    .attr("fill", function(d) { return d.to.name === options.club ? "#1f77b4" : "#ff7f0e"; })
     .attr("x", function(d) { return d.from.name === options.club ? x(d.to.name) + x.bandwidth() / 2 : x(d.from.name); })
     .attr("width", x.bandwidth()/2)
     .attr("y", function(d) {
+      var xSep = d.from.name === options.club ? x(d.to.name) + x.bandwidth() / 2 : x(d.from.name);
       var f = d.to.name === options.club;
       var res = f ? y(getPlayerValue(d.transfer.value)) - heightCounter[d.from.name].to : y(getPlayerValue(d.transfer.value)) - heightCounter[d.to.name].from;
       if (!f) {
@@ -205,6 +153,7 @@ function drawClubBarchartClub(svg, width, height, options) {
       } else {
         heightCounter[d.from.name].to += height - y(getPlayerValue(d.transfer.value));
       }
+      seperatorHeights.push([xSep, res]);
       return res;
     })
     // .attr("y", function(d) {
@@ -222,4 +171,11 @@ function drawClubBarchartClub(svg, width, height, options) {
     .on("mouseenter", handleMouseEnter)
     .on("mouseleave", handleMouseLeave)
     .on("mousemove", handleMouseMove);
+
+  svg.selectAll("bar").data(seperatorHeights).enter().append("line")
+    .style("stroke", "black")  // colour the line
+    .attr("x1", function(d){return d[0];})     // x position of the first end of the line
+    .attr("y1", function(d){return d[1];})      // y position of the first end of the line
+    .attr("x2", function(d){return d[0] + x.bandwidth() / 2;})     // x position of the second end of the line
+    .attr("y2", function(d){return d[1];});    // y position of the second end of the line
 }
